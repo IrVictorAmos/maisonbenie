8c3d702ad0e78fe0c1007ba8a44b993bc8f25ed2
victorAmos12 <vh3419700gmali.com>
2026-01-22 09:47:52 +0200

    troisième PWA

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PwaPromptComponent } from './shared/components/pwa-prompt.component';
 import { InstallButtonComponent } from './shared/components/install-button.component';
-import { PwaDebugComponent } from './shared/components/pwa-debug.component';
 import { InstallInstructionsModalComponent } from './shared/components/install-instructions-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PwaPromptComponent, InstallButtonComponent, PwaDebugComponent, InstallInstructionsModalComponent],
+  imports: [RouterOutlet, PwaPromptComponent, InstallButtonComponent, InstallInstructionsModalComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
